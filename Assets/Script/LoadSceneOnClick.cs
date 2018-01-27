e911000074d23d2789691f7da74d891f78dbae4b
Michael Tang <mtang8264@gmail.com>
2018-01-27 11:07:13 -0500

    Add Load Scene on Click script

---
+++ Assets/Script/LoadSceneOnClick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+	public int sceneCode;
+
+	void OnMouseDown(){
+		SceneManager.LoadScene (sceneCode);
+	}
+}
