98c06547a0b347804fb187c08005407db9a887a0
Michael Tang <mtang8264@gmail.com>
2018-01-27 17:45:28 -0500

    Added test of Altar puzzle

---
+++ Assets/Michael_Test/BoardManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+	public BoardSlot[] board;
+	public bool letterIsPlaced = false;
+	public SpriteRenderer letter;
+
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < board.Length-1; i ++){
+			if (board [i].containsLetter) {
+				letterIsPlaced = true;
+				break;
+			}
+		}
+
+		if (letterIsPlaced) {
+			letter.enabled = false;
+		} else {
+			letter.enabled = true;
+		}
+	}
+
+	public void ClearBoard(){
+		letterIsPlaced = false;
+		for (int i = 0; i < board.Length - 1; i++) {
+			board [i].containsLetter = false;
+		}
+	}
+}
