89fd759bd01f8f479941dfb59b39356d28d2ee1e
Michael Tang <mtang8264@gmail.com>
2018-01-27 13:30:38 -0500

    Update game manager

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+	public static GameManager instance = null;
+
+	public static bool radioPuzzleDone = false;
+	public static bool letterPuzzleDone = false;
+	public static bool glassPuzzleDone = false;
+	public static bool candlePuzzleDone = false;
+	public static bool finalPuzzleDone = false;
 
 	// Use this for initialization
 	void Start () {
-		
+		if (instance == null) {
+			instance = this;
+		} else {
+			Destroy (gameObject);
+		}
+		DontDestroyOnLoad (gameObject);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	//Functions to finish each puzzle
+	void FinishRadioPuzzle(){
+		radioPuzzleDone = true;
+	}
+	void FinishLetterPuzzle(){
+		letterPuzzleDone = true;
+	}
+	void FinishGlassPuzzle(){
+		glassPuzzleDone = true;
+	}
+	void FinishCandlePuzzle(){
+		candlePuzzleDone = true;
+	}
+	void FinishFinalPuzzle(){
+		finalPuzzleDone = true;
 	}
 }
