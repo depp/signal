1273a07f32bd6d8be50b913128ea3bca6395c584
Michael Tang <mtang8264@gmail.com>
2018-01-27 15:52:51 -0500

    Made music object a singleton

--- Assets/Script/Music.cs
+++ Assets/Script/Music.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Music : MonoBehaviour {
+	public static Music instance = null;
 
 	// Use this for initialization
 	void Start () {
+		if (instance == null) {
+			instance = this;
+		} else {
+			Destroy (gameObject);
+		}
 		DontDestroyOnLoad (gameObject);
 	}
 }
