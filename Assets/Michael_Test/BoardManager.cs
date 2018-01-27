2def9cf95b42abc9b45f5a5adb44444af51cc656
Michael Tang <mtang8264@gmail.com>
2018-01-27 17:54:00 -0500

    Allowed player to leave and return the letter puzzle

--- Assets/Michael_Test/BoardManager.cs
+++ Assets/Michael_Test/BoardManager.cs
@@ -7,11 +7,22 @@ public class BoardManager : MonoBehaviour {
 	public bool letterIsPlaced = false;
 	public SpriteRenderer letter;
 
+	public int letterPosition;
+
+	void Awake(){
+		if (GameManager.letterState != -1) {
+			letterPosition = GameManager.letterState;
+			letterIsPlaced = true;
+			board [letterPosition].containsLetter = true;
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
 		for(int i = 0; i < board.Length-1; i ++){
 			if (board [i].containsLetter) {
 				letterIsPlaced = true;
+				letterPosition = i;
 				break;
 			}
 		}
@@ -20,6 +31,12 @@ public class BoardManager : MonoBehaviour {
 			letter.enabled = false;
 		} else {
 			letter.enabled = true;
+		}
+
+		if (letterIsPlaced) {
+			GameManager.letterState = letterPosition;
+		}else{
+			GameManager.letterState = -1;
 		}
 	}
 
