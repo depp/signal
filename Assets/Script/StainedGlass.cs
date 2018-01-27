afea7fb3619925565519a0cb8cb6158243de92df
Dietrich Epp <depp@zdome.net>
2018-01-27 10:27:54 -0500

    Add test scene for stained glass puzzle

---
+++ Assets/Script/StainedGlass.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StainedGlass : MonoBehaviour {
+	public ColorSet colors;
+	int colorIndex;
+	SpriteRenderer sprite;
+
+	void Start() {
+		sprite = GetComponent<SpriteRenderer>();
+	}
+
+	void OnMouseDown() {
+		var colorArray = colors.colors;
+		colorIndex++;
+		if (colorIndex >= colorArray.Length) {
+			colorIndex = 0;
+		} else if (colorIndex < 0) {
+			colorIndex = colorArray.Length - 1;
+		}
+		sprite.color = colorArray[colorIndex];
+	}
+}
