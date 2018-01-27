0e70b98c96b537fc303754a50965a9a3a8639360
Michael Tang <mtang8264@gmail.com>
2018-01-27 10:19:54 -0500

    Added Cursor Change Over Object script

---
+++ Assets/Script/CursorChangeOverObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorChangeOverObject : MonoBehaviour {
+	public static Texture2D defaultCursor;
+	public Texture2D changeTo;
+	public Vector2 hotSpot = Vector2.zero;
+	public CursorMode cursorMode = CursorMode.Auto;
+
+	void OnMouseEnter () {
+		Cursor.SetCursor (changeTo, hotSpot, cursorMode);
+	}
+	void OnMouseExit () {
+		Cursor.SetCursor (defaultCursor, hotSpot, cursorMode);
+	}
+}
