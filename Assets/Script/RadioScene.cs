6be032accc292871bb8a1a2b417afb3b43bea180
Michael Tang <mtang8264@gmail.com>
2018-01-27 16:58:01 -0500

    Made title screen lead into first screan

---
+++ Assets/Script/RadioScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioScene : MonoBehaviour {
+	public Texture2D defaultCursor;
+	public GameObject background;
+	public GameObject background2;
+	public GameObject radio;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnMouseDown(){
+		Cursor.SetCursor (defaultCursor,Vector2.zero,CursorMode.Auto);
+		radio.SetActive (true);
+		background2.SetActive (true);
+		background.SetActive (false);
+	}
+}
--- Assets/Script/TitleScreen.cs
+++ Assets/Script/TitleScreen.cs
@@ -15,6 +15,7 @@ public class TitleScreen : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Space)) {
+			SceneManager.LoadScene (1);
 			Debug.Log ("This would move to the next scene");
 		} else if (Input.anyKeyDown) {
 			no.text += "no";
