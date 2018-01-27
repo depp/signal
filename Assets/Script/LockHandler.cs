76f272f106f7fcfad541ad3d195307906db31cc9
Michael Tang <mtang8264@gmail.com>
2018-01-27 12:34:19 -0500

    Add final door system

---
+++ Assets/Script/LockHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockHandler : MonoBehaviour {
+	public LockSlot[] locks;
+	public int[] inputCombo;
+	public int[] correctCombo;
+
+	public bool completed = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < 3; i++) {
+			inputCombo [i] = locks [i].currentPosition;
+		}
+
+		if (!completed) {
+			bool test = true;
+			for (int i = 0; i < 3; i++) {
+				if (inputCombo [i] != correctCombo [i]) {
+					test = false;
+				}
+			}
+			if (test) {
+				Debug.Log ("Finished!");
+				completed = true;
+			}
+		}
+	}
+}
---
+++ Assets/Script/LockSlot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockSlot : MonoBehaviour {
+	public int currentPosition = 0;
+	public Sprite[] visuals;
+	SpriteRenderer sprite;
+	public LockHandler handler;
+
+	void Start(){
+		sprite = GetComponent<SpriteRenderer>();
+	}
+
+	void Update(){
+		sprite.sprite = visuals [currentPosition];
+	}
+
+	void OnMouseDown(){
+		if (handler.completed != true) {
+			if (currentPosition < visuals.Length - 1) {
+				currentPosition++;
+			} else {
+				currentPosition = 0;
+			}
+		}
+	}
+}
