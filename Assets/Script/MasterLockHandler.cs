8cc397fbdfddb216485dd253e0d0913a4f391ac6
Michael Tang <mtang8264@gmail.com>
2018-01-27 12:56:37 -0500

    Add Master lock to make 3x3 lock

---
+++ Assets/Script/MasterLockHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterLockHandler : MonoBehaviour {
+	public bool finished = false;
+	public LockHandler[] locks;
+
+	void Update(){
+		if (!finished) {
+			bool test = true;
+			for (int i = 0; i < locks.Length - 1; i++) {
+				if (locks [i].completed == false) {
+					test = false;
+				}
+			}
+			if (test) {
+				finished = true;
+				Debug.Log ("Woot!");
+			}
+		}
+	}
+}
