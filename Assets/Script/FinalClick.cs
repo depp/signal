5e8ae842599421e9f7b2f37e5c0d87dd3b3f87e4
Michael Tang <mtang8264@gmail.com>
2018-01-28 13:31:20 -0500

    Added end background and dialogue

---
+++ Assets/Script/FinalClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalClick : MonoBehaviour {
+
+	void OnMouseDown(){
+		Dialogue.instance.PlayScript ("End Dialog", Cut);
+	}
+
+	public static void Cut(){
+		SceneChanger.ChangeScene ("Credits");
+	}
+}
